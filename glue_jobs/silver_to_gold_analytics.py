7e6c16e32673eeb30922d37684f349f901e491b7
MatlouE <149234501+MatlouE@users.noreply.github.com>
2026-08-24 08:56:53 +0000

    glue libraries

---
+++ glue_jobs/silver_to_gold_analytics.py
@@ -0,0 +1,9 @@
+import sys
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from awsglue.dynamicframe import DynamicFrame
+
+from pyspark.sql import functions as F
+from pyspark.sql.window import Window
